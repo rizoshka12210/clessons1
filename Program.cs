cccd7fdc6a332d1944062bdcb5c69a3af27ac8e7
rizoshka12210 <rizoshka12210@gmail.com>
2026-04-07 15:19:18 +0500

    tred: task1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int max = int.MinValue;
+int x;
+    while (true)
+        {
+            x = int.Parse(Console.ReadLine());
+
+            if (x == 0)
+                break;
+
+            if (x > max)
+                max = x;
+        }
+        Console.WriteLine(max);
